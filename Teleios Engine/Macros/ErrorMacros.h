bffc77f4926e5626dcf4c2c816be9cfbbbc809c9
TheColGateMann <TheColGateMann4@gmail.com>
2024-10-02 23:34:16 +0200

    Fixing this error macro. It is not HRESULT check

--- Teleios Engine/Macros/ErrorMacros.h
+++ Teleios Engine/Macros/ErrorMacros.h
@@ -6,5 +6,5 @@
 #define THROW_LAST_ERROR throw ErrorHandler::StandardException{ (UINT32)__LINE__, __FILE__, HRESULT_FROM_WIN32(GetLastError()) }; 
 #define THROW_NOGFX		 throw ErrorHandler::NoGFXException{(UINT32)__LINE__, __FILE__};
 
-#define THROW_INTERNAL_ERROR(errorString)				 					   throw ErrorHandler::InternalException{ (UINT32)__LINE__ , __FILE__ , errorString};
-#define THROW_INTERNAL_ERROR_IF(errorString, statement)  if(statement != S_OK) throw ErrorHandler::InternalException{ (UINT32)__LINE__ , __FILE__ , errorString};
+#define THROW_INTERNAL_ERROR(errorString)				 				 throw ErrorHandler::InternalException{ (UINT32)__LINE__ , __FILE__ , errorString};
+#define THROW_INTERNAL_ERROR_IF(errorString, statement)	   if(statement) throw ErrorHandler::InternalException{ (UINT32)__LINE__ , __FILE__ , errorString};
