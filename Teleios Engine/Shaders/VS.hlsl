c55cf1134c9e9dad9838b82949dde579d3e078ff
TheColGateMann <TheColGateMann4@gmail.com>
2024-10-08 12:08:10 +0200

    Fixing color varible having wrong type

--- Teleios Engine/Shaders/VS.hlsl
+++ Teleios Engine/Shaders/VS.hlsl
@@ -4,7 +4,7 @@ struct VSOut
 	float4 position : SV_POSITION;
 }; 
 
-VSOut VSMain( float3 position : POSITION, float color : COLOR )
+VSOut VSMain( float3 position : POSITION, float3 color : COLOR )
 {
 	VSOut vsout;
 	vsout.color = color;
