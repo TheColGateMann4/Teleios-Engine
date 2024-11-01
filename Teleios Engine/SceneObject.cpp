95a085fb98aacd1722777065d204028719a993ac
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-01 16:18:59 +0100

    Speratating our Drawable functions from SceneObject class

--- Teleios Engine/SceneObject.cpp
+++ Teleios Engine/SceneObject.cpp
@@ -1,144 +1,27 @@
 #include "SceneObject.h"
-
 #include "Graphics.h"
 #include "Pipeline.h"
 
-#include "Shader.h"
-#include "BlendState.h"
-#include "RasterizerState.h"
-#include "DepthStencilState.h"
-#include "InputLayout.h"
-#include "ConstantBuffer.h"
+#include "Drawable.h"
 
-SceneObject::SceneObject(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation)
-	:
-	m_position(position),
-	m_rotation(rotation)
+void SceneObject::AddMesh(Drawable* mesh)
 {
-
-}
-
-void SceneObject::AddStaticBindable(Pipeline& pipeline, const char* bindableName)
-{
-	SegregateBindable(pipeline.GetStaticResource(bindableName));
-}
-
-void SceneObject::AddBindable(std::shared_ptr<Bindable> bindable)
-{
-	m_bindables.push_back(bindable);
-
-	SegregateBindable(bindable.get());
-}
-
-void SceneObject::SetVertexBuffer(std::shared_ptr<VertexBuffer> vertexBuffer)
-{
-	AddBindable(vertexBuffer);
-
-	m_vertexBuffer = vertexBuffer.get();
-}
-
-void SceneObject::SetIndexBuffer(std::shared_ptr<IndexBuffer> indexBuffer)
-{
-	AddBindable(indexBuffer);
-
-	m_indexBuffer = indexBuffer.get();
-}
-
-void SceneObject::SetTransformConstantBuffer(std::shared_ptr<TransformConstantBuffer> transformConstantBuffer)
-{
-	AddBindable(transformConstantBuffer);
-
-	m_transformConstantBuffer = transformConstantBuffer.get();
-}
-
-void SceneObject::SegregateBindable(Bindable* bindable)
-{
-	if (auto commandListBindable = dynamic_cast<CommandListBindable*>(bindable))
-		m_commandListBindables.push_back(commandListBindable);
-
-	if (auto directCommandListBindable = dynamic_cast<DirectCommandListBindable*>(bindable))
-		m_directCommandListBindables.push_back(directCommandListBindable);
-
-	if (auto rootSignatureBindable = dynamic_cast<RootSignatureBindable*>(bindable))
-		m_rootSignatureBindables.push_back(rootSignatureBindable);
-
-	if (auto directCommandListBindable = dynamic_cast<PipelineStateBindable*>(bindable))
-		m_pipelineStateBindables.push_back(directCommandListBindable);
+	m_meshes.push_back(mesh);
 }
 
 void SceneObject::Initialize(Graphics& graphics)
 {
-	m_bundleCommandList = std::make_unique<CommandList>(graphics, D3D12_COMMAND_LIST_TYPE_BUNDLE);
-
-	m_rootSignature = std::make_unique<RootSignature>();
-
-	// initializing root signature
-	{
-		for (auto& pRootSignatureBindable : m_rootSignatureBindables)
-			pRootSignatureBindable->BindToRootSignature(graphics, m_rootSignature.get());
-
-		m_rootSignature->Initialize(graphics);
-	}
-
-	// initialize pipeline state object
-	{
-		m_pipelineState = std::make_unique<PipelineState>();
-
-		// initializing pipeline state desc
-		{
-			for (auto& pPipelineStateBindable : m_pipelineStateBindables)
-				pPipelineStateBindable->BindToPipelineState(graphics, m_pipelineState.get());
-
-			m_pipelineState->SetRootSignature(m_rootSignature.get());
-
-			m_pipelineState->SetSampleMask(0xffffffff);
-
-			m_pipelineState->SetSampleDesc(1, 0);
-
-			m_pipelineState->SetNumRenderTargets(1);
-
-			m_pipelineState->SetRenderTargetFormat(0, graphics.GetBackBuffer()->GetFormat());
-
-			m_pipelineState->SetDepthStencilFormat(graphics.GetDepthStencil()->GetFormat());
-		}
-
-		m_pipelineState->Finish(graphics); // Finish() call gets object from desc it made up
-	}
-
-	RecordBundleList(graphics);
+	for (auto mesh : m_meshes)
+		mesh->Initialize(graphics);
 }
 
 void SceneObject::Draw(Graphics& graphics, Pipeline& pipeline) const
 {
-	CommandList* directCommandList = pipeline.GetGraphicCommandList();
-
-	directCommandList->SetPipelineState(graphics, m_pipelineState.get());
-
-	for (auto& pDirectCommandListBindable : m_directCommandListBindables)
-		pDirectCommandListBindable->BindToDirectCommandList(graphics, directCommandList);
-
-	directCommandList->ExecuteBundle(graphics, m_bundleCommandList.get());
-};
-
-void SceneObject::RecordBundleList(Graphics& graphics)
-{
-	THROW_OBJECT_STATE_ERROR_IF("BundleList was not yet initialized", !m_bundleCommandList);
-	THROW_OBJECT_STATE_ERROR_IF("PipelineState was not yet initialized", !m_pipelineState);
-	THROW_OBJECT_STATE_ERROR_IF("RootSignature was not yet initialized", !m_rootSignature);
-
-	m_bundleCommandList->Open(graphics, m_pipelineState.get());
-
-	m_bundleCommandList->SetRootSignature(graphics, m_rootSignature.get());
-
-	for (auto& pCommandListBindable : m_commandListBindables)
-		pCommandListBindable->BindToCommandList(graphics, m_bundleCommandList.get());
-
-	m_bundleCommandList->DrawIndexed(graphics, m_indexBuffer->GetIndexCount());
-
-	m_bundleCommandList->Close(graphics);
+	for (const auto mesh : m_meshes)
+		mesh->DrawDrawable(graphics, pipeline);
 }
 
-void SceneObject::Update(Graphics& graphics)
+void SceneObject::Update(Graphics& graphics, Pipeline& pipeline)
 {
 
 }
@@ -150,14 +33,7 @@ void SceneObject::DrawImguiWindow(Graphics& graphics, bool isLayerVisible)
 
 void SceneObject::UpdateTransformMatrix(Graphics& graphics, Camera& camera)
 {
-	m_transformConstantBuffer->Update(graphics, camera);
-}
 
-DirectX::XMMATRIX SceneObject::GetTransformMatrix() const
-{
-	DirectX::FXMVECTOR vecPosition = DirectX::XMLoadFloat3(&m_position);
-	DirectX::FXMVECTOR vecRotation = DirectX::XMLoadFloat3(&m_rotation);
-
-	//  multiplying position matrix by rotation matrix
-	return DirectX::XMMatrixRotationRollPitchYawFromVector(vecRotation) * DirectX::XMMatrixTranslationFromVector(vecPosition);
+	for (auto mesh : m_meshes)
+		mesh->UpdateTransformMatrix(graphics, camera);
 }
--- Teleios Engine/SceneObject.h
+++ Teleios Engine/SceneObject.h
@@ -1,77 +1,29 @@
 #pragma once
-#include "includes/DirectXIncludes.h"
-#include "includes/WRLNoWarnings.h"
+#include "includes/CppIncludes.h"
 
-#include "PipelineState.h"
-#include "CommandList.h"
-#include "RootSignature.h"
-
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "Texture.h"
-#include "ConstantBuffer.h"
-#include "TransformConstantBuffer.h"
-
-#include "Bindable.h"
-
+class Graphics;
 class Pipeline;
 class Camera;
+class Drawable;
 
 class SceneObject
 {
-protected:
-	SceneObject(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation);
-	
 public:
 	virtual ~SceneObject() = default;
 
-protected:
-	void AddStaticBindable(Pipeline& pipeline, const char* bindableName);
-
-	void AddBindable(std::shared_ptr<Bindable> bindable);
-
-	void SetVertexBuffer(std::shared_ptr<VertexBuffer> vertexBuffer);
-
-	void SetIndexBuffer(std::shared_ptr<IndexBuffer> indexBuffer);
-
-	void SetTransformConstantBuffer(std::shared_ptr<TransformConstantBuffer> transformConstantBuffer);
-
-private:
-	void SegregateBindable(Bindable* bindable);
-
 public:
-	void Initialize(Graphics& graphics);
+	void AddMesh(Drawable* mesh);
+	
+	virtual void Initialize(Graphics& graphics);
+	
+	virtual void Draw(Graphics& graphics, Pipeline& pipeline) const;
 
-	void RecordBundleList(Graphics& graphics);
-
-	void Draw(Graphics& graphics, Pipeline& pipeline) const;
-
-	virtual void Update(Graphics& graphics);
+	virtual void Update(Graphics& graphics, Pipeline& pipeline);
 
 	virtual void DrawImguiWindow(Graphics& graphics, bool isLayerVisible);
 
-	void UpdateTransformMatrix(Graphics& graphics, Camera& camera);
+	virtual void UpdateTransformMatrix(Graphics& graphics, Camera& camera);
 
-	DirectX::XMMATRIX GetTransformMatrix() const;
-
-protected:
-	std::unique_ptr<CommandList> m_bundleCommandList;
-	std::unique_ptr<PipelineState> m_pipelineState;
-	std::unique_ptr<RootSignature> m_rootSignature;
-
-protected:
-	std::vector<std::shared_ptr<Bindable>> m_bindables;
-
-	std::vector<CommandListBindable*> m_commandListBindables;
-	std::vector<DirectCommandListBindable*> m_directCommandListBindables;
-	std::vector<RootSignatureBindable*> m_rootSignatureBindables;
-	std::vector<PipelineStateBindable*> m_pipelineStateBindables;
-
-	VertexBuffer* m_vertexBuffer = nullptr;
-	IndexBuffer* m_indexBuffer = nullptr;
-	TransformConstantBuffer* m_transformConstantBuffer = nullptr;
-
-protected:
-	DirectX::XMFLOAT3 m_position = { 0.0f, 0.0f, 0.0f };
-	DirectX::XMFLOAT3 m_rotation = { 0.0f, 0.0f, 0.0f };
+private:
+	std::vector<Drawable*> m_meshes;
 };
