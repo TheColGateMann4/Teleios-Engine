b6a77ca15d8bec5c1e62b3e97177cd018395ff94
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-03 01:20:09 +0100

    Added Dynamic Vertex functionality

---
+++ Teleios Engine/DynamicVertex.cpp
@@ -0,0 +1,164 @@
+#include "DynamicVertex.h"
+#include "includes/DirectXIncludes.h"
+
+/*
+*					Dynamic Vertex Layout
+*/
+
+size_t DynamicVertex::DynamicVertexLayout::GetSize() const
+{
+	return m_size;
+}
+
+size_t DynamicVertex::DynamicVertexLayout::GetNumElements() const
+{
+	return m_numElements;
+}
+
+DynamicVertex::DynamicVertexLayout DynamicVertex::DynamicVertexLayout::Finish()
+{
+	m_finished = true;
+
+	return *this;
+}
+
+std::vector<D3D12_INPUT_ELEMENT_DESC> DynamicVertex::DynamicVertexLayout::GetInputLayout() const
+{
+	std::vector<D3D12_INPUT_ELEMENT_DESC> result;
+
+	for(const auto& layoutElement : m_elements)
+	{
+		D3D12_INPUT_ELEMENT_DESC elementDesc = {};
+		elementDesc.SemanticName = GetElementSemanticName(layoutElement.type);
+		elementDesc.SemanticIndex = 0;
+		elementDesc.Format = GetElementFormat(layoutElement.type);
+		elementDesc.InputSlot = 0;
+		elementDesc.AlignedByteOffset = layoutElement.offset;
+		elementDesc.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+		elementDesc.InstanceDataStepRate = 0;
+
+		result.push_back(elementDesc);
+	}
+
+	return result;
+}
+
+const char* DynamicVertex::DynamicVertexLayout::GetElementSemanticName(ElementType elementType)
+{
+	switch (elementType)
+	{
+		case ElementType::Position:
+			return ElementMap<ElementType::Position>::semantic;
+
+		case ElementType::Normal:
+			return ElementMap<ElementType::Normal>::semantic;
+
+		case ElementType::TextureCoords:
+			return ElementMap<ElementType::TextureCoords>::semantic;
+
+		case ElementType::Tangent:
+			return ElementMap<ElementType::Tangent>::semantic;
+
+		case ElementType::Bitangent:
+			return ElementMap<ElementType::Bitangent>::semantic;
+
+		case ElementType::Color3:
+			return ElementMap<ElementType::Color3>::semantic;
+
+		case ElementType::Color4:
+			return ElementMap<ElementType::Color4>::semantic;
+
+		default:
+		{
+			THROW_INTERNAL_ERROR("Layout type element was not handled");
+		}
+	}
+}
+
+DXGI_FORMAT DynamicVertex::DynamicVertexLayout::GetElementFormat(ElementType elementType)
+{
+	switch (elementType)
+	{
+		case ElementType::Position:
+			return ElementMap<ElementType::Position>::format;
+
+		case ElementType::Normal:
+			return ElementMap<ElementType::Normal>::format;
+
+		case ElementType::TextureCoords:
+			return ElementMap<ElementType::TextureCoords>::format;
+
+		case ElementType::Tangent:
+			return ElementMap<ElementType::Tangent>::format;
+
+		case ElementType::Bitangent:
+			return ElementMap<ElementType::Bitangent>::format;
+
+		case ElementType::Color3:
+			return ElementMap<ElementType::Color3>::format;
+
+		case ElementType::Color4:
+			return ElementMap<ElementType::Color4>::format;
+
+		default:
+		{
+			THROW_INTERNAL_ERROR("Layout type element was not handled");
+		}
+	}
+}
+
+/*
+*					Vertex
+*/
+
+DynamicVertex::DynamicVertex::Vertex::Vertex(DynamicVertexLayout& layout, void* vertexData)
+	:
+	m_layout(layout),
+	m_vertexData(vertexData)
+{
+
+}
+
+/*
+*					Dynamic Vertex
+*/ 
+
+DynamicVertex::DynamicVertex::DynamicVertex(DynamicVertexLayout layout, size_t sizeToAllocate)
+	:
+	m_layout(layout.Finish()),
+	m_numVertices(-1),
+	m_allocatedSize(sizeToAllocate),
+	m_data(new char[sizeToAllocate * m_layout.GetSize()])
+{
+
+}
+
+DynamicVertex::DynamicVertex::~DynamicVertex()
+{
+	delete[] m_data;
+}
+
+void DynamicVertex::DynamicVertex::EmplaceBack()
+{
+	m_numVertices++;
+}
+
+DynamicVertex::DynamicVertex::Vertex DynamicVertex::DynamicVertex::Back()
+{
+	return Vertex{ m_layout, static_cast<char*>(m_data) + m_layout.GetSize() * m_numVertices };
+}
+
+void* DynamicVertex::DynamicVertex::GetData()
+{
+	return m_data;
+}
+
+int DynamicVertex::DynamicVertex::GetNumVertices()
+{
+	return m_numVertices;
+}
+
+const DynamicVertex::DynamicVertexLayout& DynamicVertex::DynamicVertex::GetLayout() const
+{
+	return m_layout;
+}
---
+++ Teleios Engine/DynamicVertex.h
@@ -0,0 +1,207 @@
+#pragma once
+#include "includes/CppIncludes.h"
+#include "includes/DirectXIncludes.h"
+#include "macros/ErrorMacros.h"
+
+struct D3D12_INPUT_ELEMENT_DESC;
+
+namespace DynamicVertex
+{
+	enum class ElementType
+	{
+		Position,
+		Normal,
+		Tangent,
+		Bitangent,
+		TextureCoords,
+		Color3,
+		Color4
+	};
+
+	template<ElementType type>
+	struct ElementMap
+	{
+		bool valid = false;
+	};
+
+	template<>
+	struct ElementMap<ElementType::Position>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT3;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32_FLOAT;
+		static constexpr const char* semantic = "POSITION";
+	};
+	template<>
+	struct ElementMap<ElementType::Normal>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT3;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32_FLOAT;
+		static constexpr const char* semantic = "NORMAL";
+	};
+	template<>
+	struct ElementMap<ElementType::Tangent>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT3;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32_FLOAT;
+		static constexpr const char* semantic = "TANGENT";
+	};
+	template<>
+	struct ElementMap<ElementType::Bitangent>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT3;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32_FLOAT;
+		static constexpr const char* semantic = "BITANGENT";
+	};
+	template<>
+	struct ElementMap<ElementType::TextureCoords>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT2;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32_FLOAT;
+		static constexpr const char* semantic = "TEXCOORDS";
+	};
+	template<>
+	struct ElementMap<ElementType::Color3>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT3;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32_FLOAT;
+		static constexpr const char* semantic = "COLOR";
+	};
+	template<>
+	struct ElementMap<ElementType::Color4>
+	{
+		static constexpr bool valid = true;
+
+		using dataType = DirectX::XMFLOAT4;
+		static constexpr unsigned int size = sizeof(dataType);
+		static constexpr DXGI_FORMAT format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		static constexpr const char* semantic = "COLOR";
+	};
+
+
+	class DynamicVertexLayout
+	{
+		struct LayoutElement
+		{
+			ElementType type;
+			unsigned int size;
+			unsigned int offset;
+		};
+
+	public:
+		template<ElementType elementType>
+		void AddElement()
+		{
+			THROW_OBJECT_STATE_ERROR_IF("Layout was unfinished", m_finished);
+
+			LayoutElement element = {};
+			element.type = elementType;
+			element.size = GetNewElementSize<elementType>();
+			element.offset = m_size;
+
+
+			m_size += element.size;
+			m_numElements++;
+
+			m_elements.push_back(std::move(element));
+		}
+
+		template<ElementType elementType>
+		size_t GetElementOffset() const
+		{
+			THROW_OBJECT_STATE_ERROR_IF("Layout was unfinished", !m_finished);
+			THROW_OBJECT_STATE_ERROR_IF("Layout was empty", m_elements.empty());
+
+			for (const auto& element : m_elements)
+				if (element.type == elementType)
+					return element.offset;
+
+			THROW_INTERNAL_ERROR("Failed to find element for given type");
+		}
+
+		size_t GetSize() const;
+
+		size_t GetNumElements() const;
+
+		DynamicVertexLayout Finish();
+
+		std::vector<D3D12_INPUT_ELEMENT_DESC> GetInputLayout() const;
+
+	private:
+		template<ElementType elementType>
+		consteval static unsigned int GetNewElementSize()
+		{
+			return ElementMap<elementType>::size;
+		}
+
+		static const char* GetElementSemanticName(ElementType elementType);
+
+		static DXGI_FORMAT GetElementFormat(ElementType elementType);
+
+	private:
+		std::vector<LayoutElement> m_elements;
+		size_t m_numElements = 0;
+		size_t m_size = 0;
+		bool m_finished = false;
+	};
+
+	class DynamicVertex
+	{
+		class Vertex
+		{
+		public:
+			Vertex(DynamicVertexLayout& layout, void* vertexData);
+
+		public:
+			template<ElementType elementType>
+			ElementMap<elementType>::dataType& GetPropety()
+			{
+				return *reinterpret_cast<ElementMap<elementType>::dataType*>(static_cast<char*>(m_vertexData) + m_layout.GetElementOffset<elementType>());
+			}
+
+		private:
+			DynamicVertexLayout& m_layout;
+			void* m_vertexData;
+		};
+
+	public:
+		DynamicVertex(DynamicVertexLayout layout, size_t sizeToAllocate);
+
+		~DynamicVertex();
+
+	public:
+		void EmplaceBack();
+
+		Vertex Back();
+
+	public:
+		void* GetData();
+
+		int GetNumVertices();
+
+		const DynamicVertexLayout& GetLayout() const;
+
+	private:
+		DynamicVertexLayout m_layout;
+		int m_numVertices;
+		size_t m_allocatedSize;
+		void* m_data;
+	};
+};
