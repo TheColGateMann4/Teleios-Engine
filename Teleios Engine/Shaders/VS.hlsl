a7a6188d9e30231552dbee187b3b23b30f55b463
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-03 01:25:15 +0100

    Changed basic Vertex Shader to handle only position since its supposed to be simple shader

--- Teleios Engine/Shaders/VS.hlsl
+++ Teleios Engine/Shaders/VS.hlsl
@@ -5,17 +5,7 @@ cbuffer transforms : register(b0)
 	matrix transformInCameraView;
 };
 
-struct VSOut
+float4 VSMain( float3 position : POSITION) : SV_POSITION
 {
-	float2 textureCoords : TEXCOORDS;
-	float4 position : SV_POSITION;
-}; 
-
-VSOut VSMain( float3 position : POSITION, float2 textureCoords : TEXCOORDS )
-{
-	VSOut vsout;
-	vsout.textureCoords = textureCoords;
-	vsout.position = mul(transformInCameraView, float4(position, 1.0f));
-
-	return vsout;
+	return mul(transformInCameraView, float4(position, 1.0f));
 }
