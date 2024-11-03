6e2a1869b3f14faea8b11af6d999977485e5ca4d
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-03 01:22:14 +0100

    Made Input Layout use DynamicVertexLayout. This saves us ton of repetitions

--- Teleios Engine/InputLayout.cpp
+++ Teleios Engine/InputLayout.cpp
@@ -1,23 +1,13 @@
 #include "InputLayout.h"
 #include "Macros/ErrorMacros.h"
 #include "PipelineState.h"
+#include "DynamicVertex.h"
 
-InputLayout::InputLayout(std::vector<Item>& layout)
+InputLayout::InputLayout(DynamicVertex::DynamicVertexLayout& layout)
 	:
-	m_layoutElements()
+	m_layoutElements(layout.GetInputLayout())
 {
-	size_t layoutSize = layout.size();
-
-	THROW_OBJECT_STATE_ERROR_IF("Input layout cannot exceed 15 elements", layout.size() > 15);
-
-	size_t accumulatedAlignedOffset = 0;
-
-	for (const auto& item : layout)
-	{
-		m_layoutElements.push_back(GetItemDesc(item, accumulatedAlignedOffset));
-
-		accumulatedAlignedOffset += GetItemSize(item.type);
-	}
+	THROW_OBJECT_STATE_ERROR_IF("Input layout cannot exceed 15 elements", layout.GetNumElements() > 15);
 
 	m_desc = {};
 	m_desc.pInputElementDescs = m_layoutElements.data();
@@ -32,72 +22,4 @@ D3D12_INPUT_LAYOUT_DESC InputLayout::Get()
 void InputLayout::BindToPipelineState(Graphics& graphics, PipelineState* pipelineState)
 {
 	pipelineState->SetInputLayout(this);
-}
-
-D3D12_INPUT_ELEMENT_DESC InputLayout::GetItemDesc(Item item, size_t accumulatedSize)
-{
-	D3D12_INPUT_ELEMENT_DESC elementDesc = {};
-	elementDesc.SemanticName = item.semanticName;
-	elementDesc.SemanticIndex = 0;
-	elementDesc.Format = GetItemFormat(item.type);
-	elementDesc.InputSlot = 0;
-	elementDesc.AlignedByteOffset = accumulatedSize;
-	elementDesc.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
-	elementDesc.InstanceDataStepRate = 0;
-
-	return elementDesc;
-}
-
-size_t InputLayout::GetItemSize(ItemType itemType)
-{
-	switch (itemType)
-	{
-		case ItemType::TexCoords:
-			return sizeof(DirectX::XMFLOAT2);
-
-		case ItemType::Position3:
-		case ItemType::Color3:
-		case ItemType::Normal:
-			return sizeof(DirectX::XMFLOAT3);
-
-		case ItemType::Position4:
-		case ItemType::Color4:
-			return sizeof(DirectX::XMFLOAT4);
-
-		default:
-		{
-			std::string errorString = "Item Type ";
-			errorString += size_t(itemType);
-			errorString += " wasn't defined in GetItemSize";
-
-			THROW_INTERNAL_ERROR(errorString.c_str());
-		}
-	}
-}
-
-DXGI_FORMAT InputLayout::GetItemFormat(ItemType itemType)
-{
-	switch(itemType)
-	{
-		case ItemType::TexCoords:
-			return DXGI_FORMAT_R32G32_FLOAT;
-
-		case ItemType::Position3:
-		case ItemType::Color3:
-		case ItemType::Normal:
-			return DXGI_FORMAT_R32G32B32_FLOAT;
-
-		case ItemType::Position4:
-		case ItemType::Color4:
-			return DXGI_FORMAT_R32G32B32A32_FLOAT;
-
-		default:
-		{
-			std::string errorString = "Item Type ";
-			errorString += size_t(itemType);
-			errorString += " wasn't defined in GetItemFormat";
-
-			THROW_INTERNAL_ERROR(errorString.c_str());
-		}
-	}
 }
--- Teleios Engine/InputLayout.h
+++ Teleios Engine/InputLayout.h
@@ -6,37 +6,19 @@
 class Graphics;
 class PipelineState;
 
+namespace DynamicVertex
+{
+	class DynamicVertexLayout;
+};
+
 class InputLayout : public Bindable, public PipelineStateBindable
 {
 public:
-	enum class ItemType
-	{
-		Position3,
-		Position4,
-		Color3,
-		Color4,
-		TexCoords,
-		Normal
-	};
-
-	struct Item
-	{
-		const char* semanticName;
-		ItemType type;
-	};
-
-public:
-	InputLayout(std::vector<Item>& layout);
+	InputLayout(DynamicVertex::DynamicVertexLayout& layout);
 
 	D3D12_INPUT_LAYOUT_DESC Get();
 
 	virtual void BindToPipelineState(Graphics& graphics, PipelineState* pipelineState) override;
-
-private:
-	D3D12_INPUT_ELEMENT_DESC GetItemDesc(Item item,size_t accumulatedSize);
-
-	size_t GetItemSize(ItemType itemType);
-	DXGI_FORMAT GetItemFormat(ItemType itemType);
 
 private:
 	std::vector<D3D12_INPUT_ELEMENT_DESC> m_layoutElements;
