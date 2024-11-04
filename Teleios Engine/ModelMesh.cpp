61d53eb047e521bbf06ac1383f696a03cbebaea6
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-04 02:54:12 +0100

    Added handling basic textures and printing unused material propeties

--- Teleios Engine/ModelMesh.cpp
+++ Teleios Engine/ModelMesh.cpp
@@ -92,17 +92,49 @@ ModelMesh::ModelMesh(Graphics& graphics, aiMesh* mesh, aiMaterial* material, Dir
 		SetIndexBuffer(std::make_shared<IndexBuffer>(graphics, indices));
 	}
 
+	std::vector<const char*> shaderMacros = {};
+	shaderMacros.push_back("OUTPUT_CAMAERAPOS"); // phong requirement
+
 	// handle material
 	{
-		//AddBindable(std::make_shared<StaticSampler>(D3D12_FILTER_MIN_MAG_MIP_POINT));
+		ProcessMaterialPropeties(material);
 
-		// make some handler for material and make shader generator or something that would cooperate with it
+		MaterialPropeties materialPropeties = ProcessMaterialPropeties(material);
+
+		if(materialPropeties.hasAnyMap)
+		{
+			AddBindable(std::make_shared<StaticSampler>(D3D12_FILTER_MIN_MAG_MIP_POINT));
+
+			shaderMacros.push_back("TEXTURE_ANY");
+			shaderMacros.push_back("INPUT_TEXCCORDS"); // since we are handling textures, we will need texcoords argument provided to our shaders
+		}
+
+		if (materialPropeties.hasDiffuseMap)
+		{
+			AddBindable(std::make_shared<Texture>(graphics, materialPropeties.diffuseMapPath.c_str(), std::vector<TargetSlotAndShader>{{ShaderVisibilityGraphic::PixelShader, 0}}));
+			shaderMacros.push_back("TEXTURE_DIFFUSE");
+		}
+
+		if (materialPropeties.hasNormalMap)
+		{
+			AddBindable(std::make_shared<Texture>(graphics, materialPropeties.normalMapPath.c_str(), std::vector<TargetSlotAndShader>{{ShaderVisibilityGraphic::PixelShader, 1}}));
+			shaderMacros.push_back("TEXTURE_NORMAL");
+
+			shaderMacros.push_back("INPUT_TANGENT");
+			shaderMacros.push_back("INPUT_BITANGENT");
+		}
+
+		if (materialPropeties.hasSpecularMap)
+		{
+			AddBindable(std::make_shared<Texture>(graphics, materialPropeties.specularMapPath.c_str(), std::vector<TargetSlotAndShader>{{ShaderVisibilityGraphic::PixelShader, 2}}));
+			shaderMacros.push_back("TEXTURE_SPECULAR");
+		}
 	}
 
 	SetTransformConstantBuffer(std::make_shared<TransformConstantBuffer>(graphics, this));
 
-	AddBindable(std::make_shared<Shader>("PS_WhiteColor", ShaderType::PixelShader));
-	AddBindable(std::make_shared<Shader>("VS", ShaderType::VertexShader));
+	AddBindable(std::make_shared<Shader>("PS_Phong", ShaderType::PixelShader, shaderMacros));
+	AddBindable(std::make_shared<Shader>("VS", ShaderType::VertexShader, shaderMacros));
 	AddBindable(std::make_shared<InputLayout>(vertexLayout));
 
 	AddBindable(std::make_shared<BlendState>());
@@ -111,4 +143,88 @@ ModelMesh::ModelMesh(Graphics& graphics, aiMesh* mesh, aiMaterial* material, Dir
 	AddBindable(std::make_shared<PrimitiveTechnology>(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE));
 
 	AddStaticBindable("lightBuffer");
+}
+
+ModelMesh::MaterialPropeties ModelMesh::ProcessMaterialPropeties(aiMaterial* material)
+{
+	MaterialPropeties resultPropeties;
+
+	for (size_t propetyIndex = 0; propetyIndex < material->mNumProperties; propetyIndex++)
+	{
+		aiMaterialProperty* materialPropety = material->mProperties[propetyIndex];
+
+		switch (material->mProperties[propetyIndex]->mType)
+		{
+			case aiPTI_Integer:
+			{
+				PrintMaterialPropeties<int>(materialPropety);
+				break;
+			}
+			case aiPTI_Float:
+			{
+				PrintMaterialPropeties<float>(materialPropety);
+				break;
+			}
+			case aiPTI_Double:
+			{
+				PrintMaterialPropeties<double>(materialPropety);
+				break;
+
+			}
+			case aiPTI_String:
+			{
+				std::string texturePath = std::string("Models/nanosuit/") + reinterpret_cast<aiString*>(materialPropety->mData)->C_Str();
+				unsigned int semantic = materialPropety->mSemantic;
+
+				if (semantic != aiTextureType_NONE)
+					resultPropeties.hasAnyMap = true;
+
+				switch (semantic)
+				{
+					case aiTextureType_DIFFUSE:
+					{
+						resultPropeties.hasDiffuseMap = true;
+						resultPropeties.diffuseMapPath = texturePath;
+						break;
+					}
+					case aiTextureType_NORMALS:
+					{
+						resultPropeties.hasNormalMap = true;
+						resultPropeties.normalMapPath = texturePath;
+						break;
+					}
+					case aiTextureType_SPECULAR:
+					{
+						resultPropeties.hasSpecularMap = true;
+						resultPropeties.specularMapPath = texturePath;
+						break;
+					}
+					default: // if we are not handling the map we will print it in console
+					{
+						std::cout << "index: " << materialPropety->mIndex << '\n';
+						std::cout << "key: " << materialPropety->mKey.C_Str() << '\n';
+						std::cout << "semantic: " << semantic << '\n';
+						std::cout << "data: \"" << texturePath << "\"\n";
+						break;
+					}
+				}
+
+				break;
+			}
+		}
+
+		std::cout << "\n\n\n";
+	}
+
+	std::cout << "\n\n\n\n\n\n\n\n";
+
+	return resultPropeties;
+}
+
+void ModelMesh::GetMaterialPropeties(aiMaterialProperty* materialPropety, void*& baseData, unsigned int& dataLength, unsigned int& index, const char*& key)
+{
+	baseData = materialPropety->mData;
+	dataLength = materialPropety->mDataLength;
+	index = materialPropety->mIndex;
+	key = materialPropety->mKey.C_Str();
 }
--- Teleios Engine/ModelMesh.h
+++ Teleios Engine/ModelMesh.h
@@ -5,11 +5,49 @@ class Graphics;
 
 class aiMesh;
 class aiMaterial;
+class aiMaterialProperty;
 
 class ModelMesh : public Drawable
 {
+	struct MaterialPropeties
+	{
+		bool hasAnyMap = false;
+
+		bool hasDiffuseMap = false;
+		bool hasNormalMap = false;
+		bool hasSpecularMap = false;
+
+		std::string diffuseMapPath;
+		std::string normalMapPath;
+		std::string specularMapPath;
+	};
+
 public:
 	ModelMesh(Graphics& graphics, aiMesh* mesh, aiMaterial* material, DirectX::XMFLOAT3 position = { -2.0f, 0.0f, 0.0f }, DirectX::XMFLOAT3 rotation = { 0.0f, 0.0f, 0.0f });
 
+private:
+	MaterialPropeties ProcessMaterialPropeties(aiMaterial* material);
+
+	template<class T>
+	static void PrintMaterialPropeties(aiMaterialProperty* materialPropety)
+	{
+		void* baseData;
+		unsigned int dataLength;
+		unsigned int index;
+		const char* key;
+
+		GetMaterialPropeties(materialPropety, baseData, dataLength, index, key);
+
+		T* pData = reinterpret_cast<T*>(baseData);
+		size_t numElements = dataLength / sizeof(T);
+
+		std::cout << "index: " << index << '\n';
+		std::cout << "key: " << key << '\n';
+
+		for (size_t dataIndex = 0; dataIndex < numElements; dataIndex++)
+			std::cout << "data" << dataIndex << " : " << pData[dataIndex] << '\n';
+	}
+
+	static void GetMaterialPropeties(aiMaterialProperty* materialPropety, void*& baseData, unsigned int& dataLength, unsigned int& index, const char*& key);
 };
 
