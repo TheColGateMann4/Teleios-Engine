4c20f1dc59527ae0c8fb5a5af14a2b8da8844795
TheColGateMann <TheColGateMann4@gmail.com>
2024-11-22 19:55:02 +0100

    We don't need to initialize slot and target shader in static sampler desc since it will be written from target vector

--- Teleios Engine/Sampler.cpp
+++ Teleios Engine/Sampler.cpp
@@ -17,9 +17,7 @@ StaticSampler::StaticSampler(D3D12_FILTER filter, D3D12_TEXTURE_ADDRESS_MODE ove
 	m_staticSamplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK;
 	m_staticSamplerDesc.MinLOD = 0;
 	m_staticSamplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
-	m_staticSamplerDesc.ShaderRegister = targets.front().slot;
 	m_staticSamplerDesc.RegisterSpace = 0;
-	m_staticSamplerDesc.ShaderVisibility = D3D12_SHADER_VISIBILITY(targets.front().target);
 }
 
 std::shared_ptr<StaticSampler> StaticSampler::GetBindableResource(D3D12_FILTER filter, D3D12_TEXTURE_ADDRESS_MODE overlappingMode, std::vector<TargetSlotAndShader> targets)
