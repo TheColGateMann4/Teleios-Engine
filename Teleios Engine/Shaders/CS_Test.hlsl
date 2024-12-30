b7bb9f3401c62334e1d52ee830b7d24ade63505e
TheColGateMann <TheColGateMann4@gmail.com>
2024-12-30 21:11:23 +0100

    Updated compute shader for mip mapping

--- Teleios Engine/Shaders/CS_Test.hlsl
+++ Teleios Engine/Shaders/CS_Test.hlsl
@@ -8,19 +8,21 @@ cbuffer data : register(b0)
     uint b_LevelOfDetail;
 }
 
+// handle mip maps that have odd number of pixels
+// translate SRGB color space to normal one
 [numthreads(1, 1, 1)]
 void CSMain( uint3 DTid : SV_DispatchThreadID )
 {
-    uint bufferWidth, bufferHeight;
-    outBuffer.GetDimensions(bufferWidth, bufferHeight);
+    uint outBufferWidth, outBufferHeight;
+    outBuffer.GetDimensions(outBufferWidth, outBufferHeight);
 
-    float4 firstSample = t_texture.SampleLevel(s_sampler, uint2(bufferWidth, bufferHeight), 0);
+    float2 pixelRatio = float2(1.0f / outBufferWidth, 1.0f / outBufferHeight);
 
-    for(uint y = 0; y < bufferHeight; y++)
+    for(uint y = 0; y < outBufferWidth; y++)
     {
-        for(uint x = 0; x < bufferWidth; x++)
+        for(uint x = 0; x < outBufferHeight; x++)
         {
-            outBuffer[uint2(x,y)] = firstSample;
+            outBuffer[uint2(x,y)] = t_texture.SampleLevel(s_sampler, float2(pixelRatio.x * x + pixelRatio.x / 2, pixelRatio.y * y + pixelRatio.y / 2), b_LevelOfDetail);
         }
     }
 }
