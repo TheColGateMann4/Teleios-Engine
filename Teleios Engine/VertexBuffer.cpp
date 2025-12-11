c0135ef88f42c96644ca97c723122d3be8d9c445
Colgate <Colgate@DESKTOP-R0M5L15>
2025-12-11 15:50:52 +0100

    Updated VertexBuffer to use new Buffer functioanlity

--- Teleios Engine/VertexBuffer.cpp
+++ Teleios Engine/VertexBuffer.cpp
@@ -1,42 +1,61 @@
 #include "VertexBuffer.h"
 #include "Macros/ErrorMacros.h"
-#include "Graphics.h"
-#include "CommandList.h"
 
+#include "Graphics.h"
 #include "Pipeline.h"
 
 #include "DynamicVertex.h"
 
 #include "BindableResourceList.h"
 
-VertexBuffer::VertexBuffer(Graphics& graphics, DynamicVertex::DynamicVertex& dynamicVertexBuffer)
-	:
-	VertexBuffer(graphics, dynamicVertexBuffer.GetData(), dynamicVertexBuffer.GetNumVertices(), dynamicVertexBuffer.GetLayout().GetSize())
-{
-
-}
-
 VertexBuffer::VertexBuffer(Graphics& graphics, void* pData, size_t numElements, size_t dataStride)
 {
-	m_buffer = std::make_shared<Buffer>(graphics, numElements * dataStride, DXGI_FORMAT_UNKNOWN, Buffer::CPUAccess::unknown, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
-	m_uploadBuffer = std::make_shared<Buffer>(graphics, numElements * dataStride, DXGI_FORMAT_UNKNOWN, Buffer::CPUAccess::write);
+	m_buffer = std::make_shared<Buffer>(graphics, numElements, dataStride, Buffer::CPUAccess::unknown, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
+	m_uploadBuffer = std::make_shared<Buffer>(graphics, numElements, dataStride, Buffer::CPUAccess::write);
 
 	m_vertexBufferView.BufferLocation = m_buffer->GetResource()->GetGPUVirtualAddress();
 	m_vertexBufferView.SizeInBytes = numElements * dataStride;
 	m_vertexBufferView.StrideInBytes = dataStride;
 
 	// passing data to vetex buffer resource
-	UpdateBufferData(graphics, pData);
+	if (pData != nullptr)
+		UpdateBufferData(graphics, pData);
+}
+
+VertexBuffer::VertexBuffer(Graphics& graphics, DynamicVertex::DynamicVertex& dynamicVertexBuffer)
+	:
+	VertexBuffer(graphics, dynamicVertexBuffer.GetData(), dynamicVertexBuffer.GetNumVertices(), dynamicVertexBuffer.GetLayout().GetSize())
+{
+	m_layout = dynamicVertexBuffer.GetLayout();
+}
+
+VertexBuffer::VertexBuffer(Graphics& graphics, const DynamicVertex::DynamicVertexLayout& layout, unsigned int numElements)
+	:
+	VertexBuffer(graphics, nullptr, numElements, layout.GetSize())
+{
+	m_layout = layout;
+}
+
+VertexBuffer::VertexBuffer(Graphics& graphics, void* pData, const DynamicVertex::DynamicVertexLayout& layout, unsigned int numElements)
+	:
+	VertexBuffer(graphics, pData, numElements, layout.GetSize())
+{
+	m_layout = layout;
 }
 
 std::shared_ptr<VertexBuffer> VertexBuffer::GetBindableResource(Graphics& graphics, std::string identifier, DynamicVertex::DynamicVertex& dynamicVertexBuffer)
 {
-	return GetBindableResource(graphics, identifier, dynamicVertexBuffer.GetData(), dynamicVertexBuffer.GetNumVertices(), dynamicVertexBuffer.GetLayout().GetSize());
+	return BindableResourceList::GetBindableResourceByID<VertexBuffer>(graphics, "VertexBuffer#" + identifier, dynamicVertexBuffer);
 }
 
-std::shared_ptr<VertexBuffer> VertexBuffer::GetBindableResource(Graphics& graphics, std::string identifier, void* pData, size_t numElements, size_t dataStride)
+std::shared_ptr<VertexBuffer> VertexBuffer::GetBindableResource(Graphics& graphics, std::string identifier, const DynamicVertex::DynamicVertexLayout& layout, size_t numElements)
 {
-	return BindableResourceList::GetBindableResourceByID<VertexBuffer>(graphics, "VertexBuffer#" + identifier, pData, numElements, dataStride);
+	return BindableResourceList::GetBindableResourceByID<VertexBuffer>(graphics, "VertexBuffer#" + identifier, layout, numElements);
+}
+
+std::shared_ptr<VertexBuffer> VertexBuffer::GetBindableResource(Graphics& graphics, std::string identifier, void* pData, const DynamicVertex::DynamicVertexLayout& layout, size_t numElements)
+{
+	return BindableResourceList::GetBindableResourceByID<VertexBuffer>(graphics, "VertexBuffer#" + identifier, pData, layout, numElements);
 }
 
 void VertexBuffer::BindToCopyPipelineIfNeeded(Graphics& graphics, Pipeline& pipeline)
@@ -57,23 +76,28 @@ void VertexBuffer::BindToCommandList(Graphics& graphics, CommandList* commandLis
 void VertexBuffer::Update(Graphics& graphics, void* pData, size_t numElements, size_t dataStride)
 {
 	// if sizes are not the same, we will be creating new GPU resource.
-	if (m_buffer->GetSize() != numElements * dataStride)
+	if (m_buffer->GetByteSize() != numElements * dataStride)
 	{
 		graphics.GetFrameResourceDeleter()->DeleteResource(graphics, std::move(m_buffer));
-		m_buffer = std::make_shared<Buffer>(graphics, numElements * dataStride, DXGI_FORMAT_UNKNOWN, Buffer::CPUAccess::unknown, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+		m_buffer = std::make_shared<Buffer>(graphics, numElements, dataStride, Buffer::CPUAccess::unknown, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
 
 		m_vertexBufferView.BufferLocation = m_buffer->GetResource()->GetGPUVirtualAddress();
 		m_vertexBufferView.SizeInBytes = numElements * dataStride;
 		m_vertexBufferView.StrideInBytes = dataStride;
 	}
 
-	m_uploadBuffer = std::make_shared<Buffer>(graphics, numElements * dataStride, DXGI_FORMAT_UNKNOWN, Buffer::CPUAccess::write);
+	m_uploadBuffer = std::make_shared<Buffer>(graphics, numElements, dataStride, Buffer::CPUAccess::write);
 	UpdateBufferData(graphics, pData);
 }
 
 Buffer* VertexBuffer::GetBuffer()
 {
 	return m_buffer.get();
+}
+
+const DynamicVertex::DynamicVertexLayout& VertexBuffer::GetLayout() const
+{
+	return m_layout;
 }
 
 void VertexBuffer::UpdateBufferData(Graphics& graphics, void* pData)
@@ -86,7 +110,7 @@ void VertexBuffer::UpdateBufferData(Graphics& graphics, void* pData)
 
 	D3D12_RANGE writeRange = {};
 	writeRange.Begin = 0;
-	writeRange.End = m_uploadBuffer->GetSize();
+	writeRange.End = m_uploadBuffer->GetByteSize();
 
 	void* pMappedData = nullptr;
 
@@ -96,7 +120,7 @@ void VertexBuffer::UpdateBufferData(Graphics& graphics, void* pData)
 		&pMappedData
 	));
 
-	memcpy_s(pMappedData, m_uploadBuffer->GetSize(), pData, m_uploadBuffer->GetSize());
+	memcpy_s(pMappedData, m_uploadBuffer->GetByteSize(), pData, m_uploadBuffer->GetByteSize());
 
 	m_uploadBuffer->GetResource()->Unmap(0, &writeRange);
 }
