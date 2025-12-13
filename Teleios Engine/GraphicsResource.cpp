768aa34f08c4d242c900d78203e433b1d4b4569a
Colgate <Colgate@DESKTOP-R0M5L15>
2025-12-13 22:52:13 +0100

    Added GraphicsResource base class

---
+++ Teleios Engine/GraphicsResource.cpp
@@ -0,0 +1,95 @@
+#include "GraphicsResource.h"
+#include "Macros/ErrorMacros.h"
+
+#include "CommandList.h"
+
+GraphicsResource::GraphicsResource(CPUAccess cpuAccess, D3D12_RESOURCE_STATES targetState)
+	:
+	m_cpuAccess(cpuAccess),
+	m_state(D3D12_RESOURCE_STATE_COMMON),
+	m_targetState(targetState)
+{
+
+}
+
+ID3D12Resource* GraphicsResource::GetResource() const
+{
+	return m_pResource.Get();
+}
+
+void GraphicsResource::CopyResourcesTo(Graphics& graphics, CommandList* copyCommandList, GraphicsResource* dst)
+{
+	THROW_INTERNAL_ERROR_IF("Dest resource was NULL", dst == nullptr);
+
+	BEGIN_COMMAND_LIST_EVENT(copyCommandList, "Copying GraphicsTexture");
+
+	copyCommandList->SetResourceState(graphics, this, D3D12_RESOURCE_STATE_COPY_SOURCE);
+	copyCommandList->SetResourceState(graphics, dst, D3D12_RESOURCE_STATE_COPY_DEST);
+
+	copyCommandList->CopyResource(graphics, dst->GetResource(), this->GetResource());
+
+	copyCommandList->SetResourceState(graphics, dst, dst->GetResourceTargetState());
+	copyCommandList->SetResourceState(graphics, this, this->GetResourceTargetState());
+
+	END_COMMAND_LIST_EVENT(copyCommandList);
+}
+
+
+D3D12_RESOURCE_STATES GraphicsResource::GetResourceState() const
+{
+	return m_state;
+}
+
+D3D12_RESOURCE_STATES GraphicsResource::GetResourceTargetState() const
+{
+	return m_targetState;
+}
+
+void GraphicsResource::SetResourceState(D3D12_RESOURCE_STATES newState)
+{
+	m_state = newState;
+}
+
+GraphicsResource::CPUAccess GraphicsResource::GetCPUAccess() const
+{
+	return m_cpuAccess;
+}
+
+D3D12_CPU_PAGE_PROPERTY GraphicsResource::GetHardwareHeapUsagePropety(CPUAccess cpuAccess)
+{
+	switch (cpuAccess)
+	{
+	case CPUAccess::readwrite:		return D3D12_CPU_PAGE_PROPERTY_WRITE_COMBINE;
+	case CPUAccess::write:			return D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
+	case CPUAccess::notavailable:	return D3D12_CPU_PAGE_PROPERTY_NOT_AVAILABLE;
+	case CPUAccess::unknown:		return D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+
+	default:  return D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	}
+}
+
+D3D12_MEMORY_POOL GraphicsResource::GetHardwareHeapMemoryPool(CPUAccess cpuAccess)
+{
+	switch (cpuAccess)
+	{
+	case CPUAccess::readwrite:		return D3D12_MEMORY_POOL_L0;
+	case CPUAccess::write:			return D3D12_MEMORY_POOL_L0;
+	case CPUAccess::notavailable:	return D3D12_MEMORY_POOL_L1;
+	case CPUAccess::unknown:		return D3D12_MEMORY_POOL_UNKNOWN;
+
+	default:  return D3D12_MEMORY_POOL_UNKNOWN;
+	}
+}
+
+D3D12_HEAP_TYPE GraphicsResource::GetHardwareHeapType(CPUAccess cpuAccess)
+{
+	switch (cpuAccess)
+	{
+	case CPUAccess::readwrite:		return D3D12_HEAP_TYPE_CUSTOM;
+	case CPUAccess::write:			return D3D12_HEAP_TYPE_CUSTOM;
+	case CPUAccess::notavailable:	return D3D12_HEAP_TYPE_CUSTOM;
+	case CPUAccess::unknown:		return D3D12_HEAP_TYPE_DEFAULT;
+
+	default:  return D3D12_HEAP_TYPE_DEFAULT;
+	}
+}
---
+++ Teleios Engine/GraphicsResource.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "includes/CppIncludes.h"
+#include "includes/DirectXIncludes.h"
+#include "includes/WRLNoWarnings.h"
+
+class Graphics;
+class CommandList;
+
+class GraphicsResource
+{
+public:
+	enum class CPUAccess
+	{
+		readwrite,
+		write,
+		notavailable,
+		unknown
+	};
+
+protected:
+	GraphicsResource(CPUAccess cpuAccess, D3D12_RESOURCE_STATES targetState);
+
+public:
+	virtual ~GraphicsResource() = default;
+	GraphicsResource(GraphicsResource&&) noexcept = default;
+
+public:
+	ID3D12Resource* GetResource() const;
+
+	void CopyResourcesTo(Graphics& graphics, CommandList* copyCommandList, GraphicsResource* dst);
+
+public:
+	D3D12_RESOURCE_STATES GetResourceState() const;
+	D3D12_RESOURCE_STATES GetResourceTargetState() const;
+	void SetResourceState(D3D12_RESOURCE_STATES newState);
+
+	CPUAccess GetCPUAccess() const;
+
+protected:
+	static D3D12_CPU_PAGE_PROPERTY GetHardwareHeapUsagePropety(CPUAccess cpuAccess);
+	static D3D12_MEMORY_POOL GetHardwareHeapMemoryPool(CPUAccess cpuAccess);
+	static D3D12_HEAP_TYPE GetHardwareHeapType(CPUAccess cpuAccess);
+
+protected:
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_pResource;
+	CPUAccess m_cpuAccess;
+	D3D12_RESOURCE_STATES m_state;
+	D3D12_RESOURCE_STATES m_targetState;
+};
