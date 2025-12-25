2daacee58ee45136b4c0ddf8002258c4e6041c58
TheColGateMann4 <TheColGateMann4@gmail.com>
2025-12-25 16:40:24 +0100

    Added StandaloneMesh class for immediate draws without binding to whole job system

---
+++ Src/Scene/StandaloneMesh.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Includes/CppIncludes.h"
+
+#include "Graphics/Core/PipelineState.h"
+#include "Graphics/Core/CommandList.h"
+#include "Graphics/Core/RootSignature.h"
+#include "Graphics/Core/BindableContainer.h"
+
+#include "Includes/BindablesInclude.h"
+
+class Graphics;
+class Pipeline;
+class Camera;
+
+class StandaloneMesh
+{
+public:
+	StandaloneMesh() = default;
+	StandaloneMesh(StandaloneMesh&&) noexcept = default;
+	StandaloneMesh(const StandaloneMesh&) = delete;
+
+	virtual ~StandaloneMesh() = default;
+
+public:
+	void Initialize(Graphics& graphics, Pipeline& pipeline);
+
+	void Draw(Graphics& graphics, Pipeline& pipeline) const;
+
+	void Update(Graphics& graphics, Pipeline& pipeline);
+
+	void InitializeGraphicResources(Graphics& graphics, Pipeline& pipeline);
+
+	void DrawConstantBuffers(Graphics& graphics);
+
+public: // bindable container functions
+	void AddStaticBindable(const char* bindableName);
+
+	void AddBindable(std::shared_ptr<Bindable> bindable);
+
+	void SetVertexBuffer(std::shared_ptr<VertexBuffer> vertexBuffer);
+
+	void SetIndexBuffer(std::shared_ptr<IndexBuffer> indexBuffer);
+
+	const MeshBindableContainer& GetBindableContainter() const;
+
+protected:
+	MeshBindableContainer m_bindableContainer;
+
+	std::unique_ptr<GraphicsPipelineState> m_pipelineState;
+	std::unique_ptr<RootSignature> m_rootSignature;
+};
