68e2ad91a5aa610cd5b2fae3c4d8db065f16298d
TheColGateMann4 <TheColGateMann4@gmail.com>
2026-02-01 00:36:25 +0100

    Reworked Deferred shader to use cbuffer with predefined light count from macro

--- Src/Shaders/PS_Lightning.hlsl
+++ Src/Shaders/PS_Lightning.hlsl
@@ -6,13 +6,22 @@ Texture2D t_rt2 : register(t2);
 
 Texture2D t_depth : register(t3);
 
+struct PointLightData
+{
+    float3 lightPositionInCameraSpace;
+    float3 lightDiffuseColor;
+    float attenuationQuadratic;
+    float attenuationLinear;
+    float attenuationConstant;
+};
+
+#ifndef NUM_POINTLIGHTS
+#define NUM_POINTLIGHTS 1 
+#endif
+
 cbuffer lightBuffer : register(b0)
 {
-    float3 b_lightPositionInCameraSpace;
-    float3 b_lightDiffuseColor;
-    float b_attenuationQuadratic;
-    float b_attenuationLinear;
-    float b_attenuationConstant;
+    PointLightData b_pointlights[NUM_POINTLIGHTS];
 };
 
 cbuffer transforms : register(b2)
@@ -45,9 +54,9 @@ float3 FrenselSchlick(const float3 F0, const float VdotH)
     return F0 + (1.0f - F0) * pow(1.0f - VdotH, 5.0f);
 }
 
-float GetAttenuation(const float distanceToLight)
+float GetAttenuation(const PointLightData pointlight, const float distanceToLight)
 {
-    return 1.0f / ((b_attenuationConstant) + (b_attenuationLinear * distanceToLight) + (b_attenuationQuadratic * distanceToLight * distanceToLight));
+    return 1.0f / ((pointlight.attenuationConstant) + (pointlight.attenuationLinear * distanceToLight) + (pointlight.attenuationQuadratic * distanceToLight * distanceToLight));
 }
 
 float3 GetViewPosition(float2 texcoords, float depth)
@@ -84,36 +93,46 @@ float4 PSMain(float2 textureCoords : TEXCOORDS) : SV_TARGET
     
     const float3 N = normalize(normal); // normal
     const float3 V = normalize(-viewPosition); // view
-    const float3 L = normalize(b_lightPositionInCameraSpace - viewPosition); // light
-    const float3 H = normalize(V + L); // halfway vector
     
-    const float NdotL = max(dot(N, L), 0.0f);
-    const float NdotV = max(dot(N, V), 0.0f);
-    const float NdotH = max(dot(N, H), 0.0f);
-    const float LdotN = max(dot(L, N), 0.0f);
-    const float VdotN = max(dot(V, N), 0.0f);
-    const float VdotH = max(dot(V, H), 0.0f);
+    const float NdotV = max(dot(V, N), 0.0f);
     
     float3 F0 = lerp(float3(0.04, 0.04, 0.04), diffuse, metalness);
     const float alpha = roughness * roughness;
-    const float3 Ks = FrenselSchlick(F0, VdotH);
-    const float3 Kd = (1.0f - Ks) * (1.0f - metalness);
     
-
     const float3 lambert = diffuse / _pi;
     
-    const float3 cookTorranceNumerator = NormalDistribution(alpha, NdotH) * GeometrySmith(NdotV, NdotL, roughness) * Ks;
-    const float cookTorranceDenominator = max(4.0f * VdotN * LdotN, 0.000001f);
-    const float3 cookTorrance = cookTorranceNumerator / cookTorranceDenominator;
+    float3 accumulatedLight = float3(0.0f, 0.0f, 0.0f);
     
-    const float3 DiffuseBRDF = Kd * lambert;
-    const float3 SpecularBRDF = cookTorrance;
-    
-    const float lengthOfDistanceToLight = length(b_lightPositionInCameraSpace - viewPosition);
-    const float attenuation = GetAttenuation(lengthOfDistanceToLight);
-    const float3 lightIntensity = b_lightDiffuseColor;
-    
-    float3 outgoingLight = (DiffuseBRDF + SpecularBRDF) * LdotN * attenuation * lightIntensity;
+    [unroll]
+    for (int pointLightIndex = 0; pointLightIndex < NUM_POINTLIGHTS; pointLightIndex++)
+    {
+        PointLightData pointlight = b_pointlights[pointLightIndex];
 
-    return float4(outgoingLight, 1.0f);
+        const float3 L = normalize(pointlight.lightPositionInCameraSpace - viewPosition); // light
+        const float3 H = normalize(V + L); // halfway vector
+        
+        const float NdotL = max(dot(N, L), 0.0f);
+        const float NdotH = max(dot(N, H), 0.0f);
+        const float VdotH = max(dot(V, H), 0.0f);
+        
+        const float3 Ks = FrenselSchlick(F0, VdotH);
+        const float3 Kd = (1.0f - Ks) * (1.0f - metalness);
+        
+        const float3 cookTorranceNumerator = NormalDistribution(alpha, NdotH) * GeometrySmith(NdotV, NdotL, roughness) * Ks;
+        const float cookTorranceDenominator = max(4.0f * NdotV * NdotL, 0.000001f);
+        const float3 cookTorrance = cookTorranceNumerator / cookTorranceDenominator;
+    
+        const float3 DiffuseBRDF = Kd * lambert;
+        const float3 SpecularBRDF = cookTorrance;
+    
+        const float lengthOfDistanceToLight = length(pointlight.lightPositionInCameraSpace - viewPosition);
+        const float attenuation = GetAttenuation(pointlight, lengthOfDistanceToLight);
+        const float3 lightIntensity = pointlight.lightDiffuseColor;
+    
+        float3 outgoingLight = (DiffuseBRDF + SpecularBRDF) * NdotL * attenuation * lightIntensity;
+        
+        accumulatedLight += outgoingLight;
+    }
+
+    return float4(accumulatedLight, 1.0f);
 }
