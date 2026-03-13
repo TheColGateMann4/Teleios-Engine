87238928bf2ea04fd6de42e49c435d24aa8dc3af
TheColGateMann4 <TheColGateMann4@gmail.com>
2026-03-13 22:45:44 +0100

    Added DepthStencilViewCubeMultiResource Yes, we need to simplify the names, perhaps to something like DSVCubeMultiResource or DVCCubeMS, unsure yet

--- Src/Graphics/Bindables/DepthStencilView.cpp
+++ Src/Graphics/Bindables/DepthStencilView.cpp
@@ -23,20 +23,34 @@ DepthStencilViewBase::DepthStencilViewBase(Graphics& graphics, unsigned int numD
 
 void DepthStencilViewBase::CreateDSV(Graphics& graphics, D3D12_CPU_DESCRIPTOR_HANDLE& descriptor, GraphicsTexture* texture)
 {
-	// creating view
-	{
-		D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
-		depthStencilViewDesc.Format = texture->GetFormat();
-		depthStencilViewDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
-		depthStencilViewDesc.Flags = D3D12_DSV_FLAG_NONE;
-		depthStencilViewDesc.Texture2D.MipSlice = 0;
+	D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
+	depthStencilViewDesc.Format = texture->GetFormat();
+	depthStencilViewDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	depthStencilViewDesc.Flags = D3D12_DSV_FLAG_NONE;
+	depthStencilViewDesc.Texture2D.MipSlice = 0;
 
-		THROW_INFO_ERROR(graphics.GetDeviceResources().GetDevice()->CreateDepthStencilView(
-			texture->GetResource(),
-			&depthStencilViewDesc,
-			descriptor
-		));
-	}
+	THROW_INFO_ERROR(graphics.GetDeviceResources().GetDevice()->CreateDepthStencilView(
+		texture->GetResource(),
+		&depthStencilViewDesc,
+		descriptor
+	));
+}
+
+void DepthStencilViewBase::CreateDSVArraySlice(Graphics& graphics, D3D12_CPU_DESCRIPTOR_HANDLE& descriptor, GraphicsTexture* texture, unsigned int arraySlice)
+{
+	D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
+	depthStencilViewDesc.Format = texture->GetFormat();
+	depthStencilViewDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2DARRAY;
+	depthStencilViewDesc.Flags = D3D12_DSV_FLAG_NONE;
+	depthStencilViewDesc.Texture2DArray.MipSlice = 0;
+	depthStencilViewDesc.Texture2DArray.FirstArraySlice = arraySlice;
+	depthStencilViewDesc.Texture2DArray.ArraySize = 1;
+
+	THROW_INFO_ERROR(graphics.GetDeviceResources().GetDevice()->CreateDepthStencilView(
+		texture->GetResource(),
+		&depthStencilViewDesc,
+		descriptor
+	));
 }
 
 DepthStencilView::DepthStencilView(Graphics& graphics)
@@ -77,7 +91,7 @@ DepthStencilClearValue DepthStencilView::GetClearValue() const
 	return m_texture.GetDepthStencilClearValue();
 }
 
-DepthStencilViewMultiResource::DepthStencilViewMultiResource(Graphics& graphics)
+DepthStencilViewMultiResource::DepthStencilViewMultiResource(Graphics& graphics, DirectX::XMFLOAT2 dimensions)
 	:
 	DepthStencilViewBase(graphics, graphics.GetBufferCount())
 {
@@ -85,6 +99,12 @@ DepthStencilViewMultiResource::DepthStencilViewMultiResource(Graphics& graphics)
 
 	m_textures.reserve(numBuffers);
 	m_descriptors.resize(numBuffers);
+
+	if (dimensions.x == 0.0f)
+		dimensions.x = graphics.GetWidth();
+
+	if (dimensions.y == 0.0f)
+		dimensions.y = graphics.GetHeight();
 
 	for(int i = 0; i < numBuffers; i++)
 	{
@@ -96,7 +116,6 @@ DepthStencilViewMultiResource::DepthStencilViewMultiResource(Graphics& graphics)
 		
 		CreateDSV(graphics, descriptor, m_textures.at(i).get());
 	}
-
 }
 
 const D3D12_CPU_DESCRIPTOR_HANDLE& DepthStencilViewMultiResource::GetDescriptor(Graphics& graphics) const
@@ -137,6 +156,79 @@ DXGI_FORMAT DepthStencilViewMultiResource::GetFormat() const
 }
 
 DepthStencilClearValue DepthStencilViewMultiResource::GetClearValue() const
+{
+	THROW_INTERNAL_ERROR_IF("Depth Stencil didn't own any resources", m_textures.empty());
+
+	return m_textures.front()->GetDepthStencilClearValue();
+}
+
+DepthStencilViewCubeMultiResource::DepthStencilViewCubeMultiResource(Graphics& graphics)
+	:
+	DepthStencilViewBase(graphics, graphics.GetBufferCount() * 6) // 6 faces
+{
+	unsigned int numFaces = 6;
+	unsigned int numBuffers = graphics.GetBufferCount();
+
+	m_textures.reserve(numBuffers);
+	m_descriptors.resize(numBuffers);
+
+	for (int iFrame = 0; iFrame < graphics.GetBufferCount(); iFrame++)
+	{
+		m_textures.push_back(std::make_shared<GraphicsTexture>(graphics, GraphicsTextureDimensions(graphics.GetWidth(), graphics.GetWidth(), 1, 6), DXGI_FORMAT_D24_UNORM_S8_UINT, DepthStencilClearValue(1.0f, 0), GraphicsResource::CPUAccess::notavailable, D3D12_RESOURCE_STATE_DEPTH_WRITE, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL));
+
+		FrameDescriptor& frameDescriptors = m_descriptors.at(iFrame);
+		frameDescriptors.faces.resize(numFaces);
+
+		for (unsigned int iFace = 0; iFace < numFaces; iFace++)
+		{
+			auto& face = frameDescriptors.faces.at(iFace);
+
+			face = m_descriptorHeap->GetCPUDescriptorHandleForHeapStart();
+			face.ptr += graphics.GetDeviceResources().GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV) * ((iFrame * 6) + iFace);
+
+			CreateDSVArraySlice(graphics, face, m_textures.at(iFrame).get(), iFace);
+		}
+	}
+}
+
+const D3D12_CPU_DESCRIPTOR_HANDLE& DepthStencilViewCubeMultiResource::GetDescriptor(Graphics& graphics) const
+{
+	return m_descriptors.at(graphics.GetCurrentBufferIndex()).faces.at(4); // TODO: FOR NOW ONLY +Z face is handled
+}
+
+const GraphicsTexture* DepthStencilViewCubeMultiResource::GetResource(Graphics& graphics) const
+{
+	return m_textures.at(graphics.GetCurrentBufferIndex()).get();
+}
+
+GraphicsTexture* DepthStencilViewCubeMultiResource::GetResource(Graphics& graphics)
+{
+	return m_textures.at(graphics.GetCurrentBufferIndex()).get();
+}
+
+const GraphicsTexture* DepthStencilViewCubeMultiResource::GetResource(unsigned int i) const
+{
+	return m_textures.at(i).get();
+}
+
+GraphicsTexture* DepthStencilViewCubeMultiResource::GetResource(unsigned int i)
+{	
+	return m_textures.at(i).get();
+}
+
+DepthStencilType DepthStencilViewCubeMultiResource::GetDepthStencilType() const
+{
+	return DepthStencilType::cubeMultiResource;
+}
+
+DXGI_FORMAT DepthStencilViewCubeMultiResource::GetFormat() const
+{
+	THROW_INTERNAL_ERROR_IF("Depth Stencil didn't own any resources", m_textures.empty());
+
+	return m_textures.front()->GetFormat();
+}
+
+DepthStencilClearValue DepthStencilViewCubeMultiResource::GetClearValue() const
 {
 	THROW_INTERNAL_ERROR_IF("Depth Stencil didn't own any resources", m_textures.empty());
 
--- Src/Graphics/Bindables/DepthStencilView.h
+++ Src/Graphics/Bindables/DepthStencilView.h
@@ -10,7 +10,8 @@ enum class DepthStencilType
 {
 	unknown,
 	singleResource,
-	multiResource
+	multiResource,
+	cubeMultiResource
 };
 
 class DepthStencilViewBase
@@ -32,6 +33,7 @@ public:
 
 protected:
 	void CreateDSV(Graphics& graphics, D3D12_CPU_DESCRIPTOR_HANDLE& descriptor, GraphicsTexture* texture);
+	void CreateDSVArraySlice(Graphics& graphics, D3D12_CPU_DESCRIPTOR_HANDLE& descriptor, GraphicsTexture* texture, unsigned int arraySlice);
 
 protected:
 	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_descriptorHeap = {};
@@ -63,7 +65,7 @@ private:
 class DepthStencilViewMultiResource : public DepthStencilViewBase
 {
 public:
-	DepthStencilViewMultiResource(Graphics& graphics);
+	DepthStencilViewMultiResource(Graphics& graphics, DirectX::XMFLOAT2 dimensions = {});
 
 public:
 	virtual const D3D12_CPU_DESCRIPTOR_HANDLE& GetDescriptor(Graphics& graphics) const override;
@@ -82,5 +84,35 @@ public:
 
 private:
 	std::vector<D3D12_CPU_DESCRIPTOR_HANDLE> m_descriptors;
+	std::vector<std::shared_ptr<GraphicsTexture>> m_textures;
+};
+
+class DepthStencilViewCubeMultiResource : public DepthStencilViewBase
+{
+public:
+	DepthStencilViewCubeMultiResource(Graphics& graphics);
+
+public:
+	virtual const D3D12_CPU_DESCRIPTOR_HANDLE& GetDescriptor(Graphics& graphics) const override;
+
+	virtual const GraphicsTexture* GetResource(Graphics& graphics) const override;
+	virtual GraphicsTexture* GetResource(Graphics& graphics) override;
+
+	const GraphicsTexture* GetResource(unsigned int i) const;
+	GraphicsTexture* GetResource(unsigned int i);
+
+	virtual DepthStencilType GetDepthStencilType() const override;
+
+	virtual DXGI_FORMAT GetFormat() const override;
+
+	virtual DepthStencilClearValue GetClearValue() const override;
+
+private:
+	struct FrameDescriptor
+	{
+		std::vector<D3D12_CPU_DESCRIPTOR_HANDLE> faces;
+	};
+	
+	std::vector<FrameDescriptor> m_descriptors;
 	std::vector<std::shared_ptr<GraphicsTexture>> m_textures;
 };
