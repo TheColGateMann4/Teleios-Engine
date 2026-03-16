7fba8396bb4e10a9a01b18f5f428e2d303e5e33e
TheColGateMann4 <TheColGateMann4@gmail.com>
2026-03-16 16:43:07 +0100

    Implemented new system of RasterizerState Now Rasterizer fields are divided between object's and render pass. For now its just single for each, but will expand when needed.

--- Src/Graphics/Bindables/RasterizerState.cpp
+++ Src/Graphics/Bindables/RasterizerState.cpp
@@ -2,33 +2,85 @@
 #include "Graphics/Core/PipelineState.h"
 
 #include "Graphics/Core//ResourceList.h"
+#include "Macros/ErrorMacros.h"
 
-RasterizerState::RasterizerState(Graphics& graphics, bool twoSided)
+std::string RenderPassRasterizerStateOptions::GetIdentifier()
 {
+	std::string resultString = "RenderPassRasterizerStateOptions#";
+
+	resultString += std::to_string(m_isShadowRasterizer);
+
+	return resultString;
+}
+
+bool RenderPassRasterizerStateOptions::GetIsShadowRasterizer() const
+{
+	return m_isShadowRasterizer;
+}
+
+void RenderPassRasterizerStateOptions::SetIsShadowRasterizer(bool isShadowRasterizer)
+{
+	m_isShadowRasterizer = isShadowRasterizer;
+}
+
+std::string ObjectRasterizerStateOptions::GetIdentifier()
+{
+	std::string resultString = "ObjectRasterizerStateOptions#";
+
+	resultString += std::to_string(static_cast<std::underlying_type_t<decltype(m_cullingMode)>>(m_cullingMode));
+
+	return resultString;
+}
+
+D3D12_CULL_MODE ObjectRasterizerStateOptions::GetCullMode() const
+{
+	switch (m_cullingMode)
+	{
+		case CullingMode::cull_none:
+			return D3D12_CULL_MODE_NONE;
+		case CullingMode::cull_front:
+			return D3D12_CULL_MODE_FRONT;
+		case CullingMode::cull_back:
+			return D3D12_CULL_MODE_BACK;
+	}
+
+	THROW_INTERNAL_ERROR("Unmapped culling mode");
+}
+
+void ObjectRasterizerStateOptions::SetCulling(CullingMode cullingMode)
+{
+	m_cullingMode = cullingMode;
+}
+
+RasterizerState::RasterizerState(Graphics& graphics, RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions)
+{
+	bool isShadowRasterizer = renderPassOptions.GetIsShadowRasterizer();
+
 	m_desc = {};
-	m_desc.FillMode = D3D12_FILL_MODE_SOLID; // D3D12_FILL_MODE_WIREFRAME will be cool
-	m_desc.CullMode = twoSided ? D3D12_CULL_MODE_NONE : D3D12_CULL_MODE_BACK;
+	m_desc.FillMode = D3D12_FILL_MODE_SOLID;
+	m_desc.CullMode = objectOptions.GetCullMode();
 	m_desc.FrontCounterClockwise = false;
-	m_desc.DepthBias = 0;
-	m_desc.DepthBiasClamp = 0.0f;
-	m_desc.SlopeScaledDepthBias = 0.0f;
+	m_desc.DepthBias = isShadowRasterizer ? 40 : 0;
+	m_desc.DepthBiasClamp = isShadowRasterizer ? 0.00365f : 0.0f;
+	m_desc.SlopeScaledDepthBias = isShadowRasterizer ? 3.464f : 0.0f;
 	m_desc.DepthClipEnable = true;
 	m_desc.MultisampleEnable = false;
 	m_desc.AntialiasedLineEnable = false;
-	m_desc.ForcedSampleCount = 0;			 // not forced sample count
+	m_desc.ForcedSampleCount = 0;
 	m_desc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
 }
 
-std::shared_ptr<RasterizerState> RasterizerState::GetResource(Graphics& graphics, bool twoSided)
+std::shared_ptr<RasterizerState> RasterizerState::GetResource(Graphics& graphics, RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions)
 {
-	return ResourceList::GetResource<RasterizerState>(graphics, twoSided);
+	return ResourceList::GetResource<RasterizerState>(graphics, renderPassOptions, objectOptions);
 }
 
-std::string RasterizerState::GetIdentifier(bool twoSided)
+std::string RasterizerState::GetIdentifier(RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions)
 {
 	std::string resultString = "RasterizerState#";
 
-	resultString += std::to_string(twoSided);
+	resultString += renderPassOptions.GetIdentifier();
+	resultString += objectOptions.GetIdentifier();
 
 	return resultString;
 }
--- Src/Graphics/Bindables/RasterizerState.h
+++ Src/Graphics/Bindables/RasterizerState.h
@@ -5,15 +5,48 @@
 class Graphics;
 class GraphicsPipelineState;
 
+class RenderPassRasterizerStateOptions
+{
+public:
+	std::string GetIdentifier();
+
+	bool GetIsShadowRasterizer() const;
+
+	void SetIsShadowRasterizer(bool isShadowRasterizer);
+
+private:
+	bool m_isShadowRasterizer = false;
+};
+
+enum class CullingMode
+{
+	cull_none,
+	cull_front,
+	cull_back
+};
+
+class ObjectRasterizerStateOptions
+{
+public:
+	std::string GetIdentifier();
+
+	D3D12_CULL_MODE GetCullMode() const;
+
+	void SetCulling(CullingMode cullingMode);
+
+private:
+	CullingMode m_cullingMode = CullingMode::cull_back;
+};
+
 class RasterizerState : public Bindable, public PipelineStateBindable
 {
 public:
-	RasterizerState(Graphics& graphics, bool twoSided = false);
+	RasterizerState(Graphics& graphics, RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions);
 
 public:
-	static std::shared_ptr<RasterizerState> GetResource(Graphics& graphics, bool twoSided = false);
+	static std::shared_ptr<RasterizerState> GetResource(Graphics& graphics, RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions);
 
-	static std::string GetIdentifier(bool twoSided);
+	static std::string GetIdentifier(RenderPassRasterizerStateOptions renderPassOptions, ObjectRasterizerStateOptions objectOptions);
 
 public:
 	D3D12_RASTERIZER_DESC Get();
