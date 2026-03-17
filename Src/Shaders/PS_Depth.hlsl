0a5be33c3013b2f43781150ce778ae94864cda1c
TheColGateMann4 <TheColGateMann4@gmail.com>
2026-03-17 23:18:38 +0100

    Updated PS_Depth to bindless system

--- Src/Shaders/PS_Depth.hlsl
+++ Src/Shaders/PS_Depth.hlsl
@@ -1,10 +1,19 @@
+Texture2D t_textures[] : register(t0);
 SamplerState s_sampler : register(s0);
-Texture2D t_diffuse : register(t0);
+
+float4 SampleTexture(uint id, float2 tc)
+{
+    return t_textures[id].Sample(s_sampler, tc);
+}
+
+cbuffer textureIds : register(b2)
+{
+    uint b_diffuseTextureID;
+}
 
 void PSMain(float2 textureCoords : TEXCOORDS)
 {
-    float4 diffuseSample = t_diffuse.Sample(s_sampler, textureCoords);
-    float textureOpacity = diffuseSample.a;
+    float textureOpacity = SampleTexture(b_diffuseTextureID, textureCoords).a;
     
     clip(textureOpacity - 0.99f);
 }
